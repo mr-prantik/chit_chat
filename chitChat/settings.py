feb15d75a19cb2885fc296a2594479508363bf35
mr-prantik <prantikmahata9211@gmail.com>
2024-04-11 23:50:52 +0530

    updated

--- chitChat/settings.py
+++ chitChat/settings.py
@@ -43,6 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat.apps.ChatConfig',
+    'registration.apps.RegistrationConfig',
+
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'widget_tweaks',    
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +130,20 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
